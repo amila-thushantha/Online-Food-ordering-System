814596b99b596e4270e0965e18f90d6ad6761058
amila-thushantha <thushantha680@gmail.com>
2025-09-05 11:45:58 +0530

    create navbar and home page

--- online-food-rodering-client/src/App.js
+++ online-food-rodering-client/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import { DarkTheme } from './Theme/DarkTheme';
+import Home from './components/Home/Home';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider  theme={DarkTheme}>
+      <CssBaseline/>
+     <Navbar/>
+     <Home/>
+    </ThemeProvider>
   );
 }
 
